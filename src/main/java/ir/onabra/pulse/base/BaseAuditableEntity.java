ac3848f16c5e70958ac52c658433fc90ad21aae6
ghavisheh <hossein.ghavisheh@gmail.com>
2026-07-20 18:42:02 +0330

    feat(core): add JPA auditing config and base entity classes

---
+++ src/main/java/ir/onabra/pulse/base/BaseAuditableEntity.java
@@ -0,0 +1,19 @@
+﻿package ir.onabra.pulse.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseAuditableEntity extends BaseCreatedEntity {
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+}
---
+++ src/main/java/ir/onabra/pulse/base/BaseUuidEntity.java
@@ -0,0 +1,28 @@
+﻿package ir.onabra.pulse.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseUuidEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(
+            name = "id",
+            nullable = false,
+            updatable = false,
+            columnDefinition = "uuid"
+    )
+    private UUID id;
+}
